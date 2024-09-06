65024d680e8a43894591a536040259fd1e39eb4a
hinata Takeda <210011@jc-21.jp>
2024-09-06 15:57:39 +0900

    kadai02

---
+++ src/main/java/jp/te4a/spring/boot/myapp2/App2Application.java
@@ -0,0 +1,12 @@
+package jp.te4a.spring.boot.myapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App2Application.class, args);
+    }
+}
--- src/main/java/jp/te4a/spring/boot/myapp2/app2.java
+++ src/main/java/jp/te4a/spring/boot/myapp2/app2.java
@@ -1,5 +1,8 @@
 package jp.te4a.spring.boot.myapp2;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @RestController
 public class App2
 {
